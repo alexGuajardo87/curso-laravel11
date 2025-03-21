7156e90ccbe8aafa53e18cfd5c71e2f698eb6489
Alejandro Guajardo Cruztitla <alejandro.guajardo@uas.edu.mx>
2025-03-21 15:26:48 -0700

    le quito las S de prueba

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\CentrosComputoController;
 use App\Http\Controllers\ReportesController;
 
 
-Route::get('/holasss', function (Request $request) {
+Route::get('/hola', function (Request $request) {
     return "Hola";
 })->middleware('auth:sanctum');
 
